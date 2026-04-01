2fc5d787f0d9576f531cd52b24804554444cdbe1
khava79 <havatsechoeva011@gmail.com>
2026-04-01 21:40:37 +0300

    Delete src/main/java/StrTasks directory

--- src/main/java/StrTasks/PrintNumbers.java
+++
@@ -1,8 +0,0 @@
-package StrTasks;
-
-public class PrintNumbers {
-    public static void main(String[] args) {
-        double number = 5.7893;
-        System.out.printf("%.2f\n", number);
-    }
-}
